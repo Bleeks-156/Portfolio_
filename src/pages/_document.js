c9bdd90ebe19db12dd9b6d9e92fb5cee60f551c6
Hari Shankar Babu M <harihrk28@gmail.com>
2025-04-14 20:25:53 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Force desktop-like viewport */}
+        <meta name="viewport" content="width=1024" />
+      </Head>
       <body>
         <Main />
         <NextScript />
